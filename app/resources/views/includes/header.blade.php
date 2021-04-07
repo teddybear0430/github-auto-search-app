e8ee6674077acc907260c98750c6a3ef88ca4e91
teddybear0430 <teddybear0430@hotmail.com>
2021-04-08 00:16:16 +0900

    ヘッダーナビのリンクのクラスをユーティリティ化して。

--- app/resources/views/includes/header.blade.php
+++ app/resources/views/includes/header.blade.php
@@ -1,13 +1,20 @@
 <header class="top-0 lef-0 w-full z-40 bg-white fixed border-b border-gray-200">
-  <div class="container px-6 mx-auto flex justify-between items-center">
-    <a class="navbar-brand text-3xl" href="#">GitHubリポジトリ自動検索アプリ</a>
-    <nav>
+  <div class="container max-w-screen-lg m-auto flex justify-between items-center px-6">
+    @guest
+      <a class="navbar-brand text-3xl" href="/">GitHubリポジトリ自動検索アプリ</a>
+    @endguest
+
+    @auth
+      <a class="navbar-brand text-3xl" href="/admin">GitHubリポジトリ自動検索アプリ</a>
+    @endauth
+    <nav class="flex">
       @guest
-      <a href="/login">ログイン</a>
+      <a href="/login" class="nav-link">ログイン</a>
       @endguest
 
       @auth
-      <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-blue-600" onClick="(function(){
+      <a href="{{ route('keyword.create') }}" class="nav-link">キーワードの新規登録</a>
+      <a href="#" class="nav-link" onClick="(function(){
         document.getElementById('logout-form').submit();
         return false;
         })();">ログアウト</a>
