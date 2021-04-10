28df40350c3fdd72d37c0071cbc638ea28ea1359
teddybear0430 <teddybear0430@hotmail.com>
2021-04-10 17:03:01 +0900

    管理画面にはログインしているユーザーの登録したキーワードのみ表示するように修正。

--- app/app/Http/Controllers/Admin/DashboardController.php
+++ app/app/Http/Controllers/Admin/DashboardController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\KeywordGroup;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
   /**
    * 管理画面
-   * 登録されたキーワド一覧を表示
+   * 登録されたキーワード一覧を表示
    * @return \Illuminate\Http\Response
    */
   public function index()
   {
-    $keyword_groups = KeywordGroup::orderBy('id', 'desc')->paginate(10);
+    $user_id = Auth::id();
+
+    $keyword_groups = KeywordGroup::where('user_id', $user_id)
+      ->orderBy('id', 'desc')
+      ->paginate(10);
+
     return view('admin.index', compact('keyword_groups'));
   }
 }
