23a0e22faf9c667943d0b56f3f0d710489a8b040
teddybear0430 <teddybear0430@hotmail.com>
2021-04-10 21:21:16 +0900

    リポジトリの検索処理のリファクタリングをして。

--- app/app/Services/GithubCrawlerService.php
+++ app/app/Services/GithubCrawlerService.php
@@ -4,28 +4,48 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 
-class GithubCrawlerService {
+class GithubCrawlerService 
+{
   const BASE_URL = 'https://api.github.com/search/repositories';
 
-  public function __construct(string $search_kewword)
+  /**
+   * @param 検索対象のキーワード $search_kewword
+   * @param 検索したいリポジトリの数 $search_repository_num
+   */ 
+  public function __construct(string $search_kewword, int $search_repository_num)
   {
     $this->search_keyword = $search_kewword;
+    $this->search_repository_num = $search_repository_num;
     $this->github_token = config('app.github_token');
   }
 
+  /**
+   * GitHub API経由で、リポジトリを取得する処理
+   */
   public function search_results()
   {
     try {
-      $format= self::BASE_URL . "?q=%s+in:name&sort=stars&per_page=100&page=1&access_token=%s";
+      $format = null;
+
+      // 1〜30件のリポジトリの情報を取得したいとき
+      if ($this->search_repository_num <= 30) {
+        $format= self::BASE_URL . "?q=%s+in:name&sort=stars&access_token=%s";
+      } else {
+        // 1～100件目のリポジトリ情報
+        // MEMO: 100件以上のリポジトリを取得するには、ループ内でリクエストを送る必要がありそう
+        $format= self::BASE_URL . "?q=%s+in:name&sort=stars&access_token=%s&per_page=100&page=1";
+      }
+
       $endpoint = sprintf($format, $this->search_keyword, $this->github_token);
 
       $response = Http::get($endpoint);
       $results = $response->json();
 
-      var_dump($results);
+      if (!$results) return;
+
+      return $results;
     } catch(Exception $er) {
-      var_dump($er);
-      exit();
+      throw $er;
     }
   }
 }
