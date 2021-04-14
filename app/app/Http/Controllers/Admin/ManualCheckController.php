2dcd8df13a07a588b098c34eccb8be4f4671c73e
teddybear0430 <teddybear0430@hotmail.com>
2021-04-15 00:51:19 +0900

    リファクタ。

--- app/app/Http/Controllers/Admin/ManualCheckController.php
+++ app/app/Http/Controllers/Admin/ManualCheckController.php
@@ -16,12 +16,11 @@ class ManualCheckController extends Controller
    */
   public function manual_check(int $keyword_group_id)
   {
-    $KeywordGroup = new KeywordGroup();
-    $check_keyword_group_record = $KeywordGroup::where('id', $keyword_group_id)
+    $check_keyword_group_record = KeywordGroup::where('id', $keyword_group_id)
       ->findOrFail($keyword_group_id);
 
     // チェックを行うためのフラグをチェック中に変更
-    $check_keyword_group_record->check_status = $KeywordGroup::RUNNING;
+    $check_keyword_group_record->check_status = KeywordGroup::RUNNING;
     $check_keyword_group_record->save();
 
     // // Githubのクローリングを行うジョブを実行する
