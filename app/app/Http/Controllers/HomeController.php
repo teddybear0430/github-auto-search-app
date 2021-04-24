d7165ee8391d71cc365900b8b7ead74fddde47c0
teddybear0430 <teddybear0430@hotmail.com>
2021-04-24 23:39:02 +0900

    ログインしてる時/にアクセスしたら/adminにリダイレクトするようにして。

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
   public function index()
   {
+    if (Auth::check()) return redirect('/admin');
+
     return view('index');
   }
 }
